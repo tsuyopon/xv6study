1a8452d8065abda3d3d0b06ba633d1dd9224da06
Tsuyoshi Ooishi <tsuyoppy@r2.dion.ne.jp>
2015-12-20 12:34:27 +0900

    add comment

--- sign.pl
+++ sign.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+# このファイルはMakefile中で実行されている
+
 open(SIG, $ARGV[0]) || die "open $ARGV[0]: $!";
 
 $n = sysread(SIG, $buf, 1000);
@@ -11,6 +13,8 @@ if($n > 510){
 
 print STDERR "boot block is $n bytes (max 510)\n";
 
+# 512バイトの最後のバイトがaa55だと起動ディスクとして認識されるとのこと。以下で挿入してる
+# (参考) http://msyksphinz.hatenablog.com/entry/2015/09/15/020000
 $buf .= "\0" x (510-$n);
 $buf .= "\x55\xAA";
 
